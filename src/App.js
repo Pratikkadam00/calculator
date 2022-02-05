6b7a4936373a3e6688bf99f7a0ab7b12755429eb
Pratikkadam00 <pratikadam00@gmail.com>
2022-02-05 16:16:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Calculator from './components/calculator/Calculator';
+
+function App() {
+    return (
+        <div>
+            <h1 className='heading'>Calculator</h1>
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
